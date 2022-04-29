37acd3c92aefa029ff37a6b43116d7d52d60240a
Gupta Sandeep (IFIN DES SDF SCS) <sandeep.gupta@infineon.com>
2022-04-29 12:18:09 +0530

    Adding the script to generate the tech doc

---
+++ config/sphinx/runSphinx.bat
@@ -0,0 +1,14 @@
+echo off
+
+REM Run this script to generate the Technical Doc
+REM This script expects sphinx and sphinx-rtd-theme python packages
+REM Python interpreter should be set when script is executed
+
+REM Setting up the enviornments for SPHINX
+python -m pip install -r ../../config/python/requirements.txt
+
+REM Generate and edit reStructured text (.rst) files
+sphinx-apidoc  -f  -o  ./source  ../../src
+
+REM Generate the Technical Doc in HTML format
+make html
