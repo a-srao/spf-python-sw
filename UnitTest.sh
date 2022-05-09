1b93ac00095dd8fd70cc899e91b210a0b3fc12cb
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-09 12:44:13 +0530

    using tcsh

--- UnitTest.sh
+++ UnitTest.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-echo "Hello World"
+#!/bin/tcsh
+module load python/3.9
 python -m pip install -r config/python/requirements.txt
-coverage run --omit=*/test/* --source src,src/Hello --branch -m pytest --cache-clear --junitxml pytest_shell/pytest.xml test
-coverage html -d pytest_shell/coverage_html
-coverage xml -o pytest.xml
+python -m coverage run --omit=test --source src --branch -m pytest --cache-clear --junitxml pytest_hello/pytest.xml test
+python -m coverage html -d pytest_shell/coverage_html
+python -m coverage xml -o pytest.xml
