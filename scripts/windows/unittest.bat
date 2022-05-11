6ed56d1d1b1a5ee6784d521b8b0d270bffc0ceee
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-11 16:08:09 +0530

    unittestu

--- scripts/windows/unittest.bat
+++ scripts/windows/unittest.bat
@@ -4,6 +4,6 @@ REM run this script to run the all available Python unittests
 REM this script expects to the python packages pytest and coverage to be installed
 REM for the Python interpreter which is active when this script is being run
 python -m pip install -r config/python/requirements.txt
-coverage run --omit=*/test/* --source D:\slave\workspace\ting_feature_Unit-Perf-TestTools\src  --branch -m pytest --cache-clear --junitxml build_pytest/pytest.xml D:\slave\workspace\ting_feature_Unit-Perf-TestTools\test
+coverage run --omit=*/test/* --source D:\slave\workspace\ting_feature_Unit-Perf-TestTools\src  --branch -m pytest --cache-clear --junitxml build_pytest/pytest.xml D://slave/workspace/ting_feature_Unit-Perf-TestTools/test/unit-test
 coverage html -d build_pytest/coverage_html
 coverage xml -o pytest.xml
