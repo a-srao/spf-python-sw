57550216159ad0284ff23da404e0a7ea4a4ee82a
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-11 12:26:53 +0530

    tests folder

---
+++ test/functional-Test/test_API-Test.py
@@ -0,0 +1,6 @@
+import requests
+
+
+def test_get_locations_for_us_90210_check_status_code_equals_200():
+     response = requests.get("https://reqres.in/api/products/3")
+     assert response.status_code == 200
