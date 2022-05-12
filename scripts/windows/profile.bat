410741f5551140649f9786f87e6a3bbfc59261b2
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-12 12:22:00 +0530

    profile

--- scripts/windows/profile.bat
+++ scripts/windows/profile.bat
@@ -1,3 +1,3 @@
 echo off
-python -m cProfile -o profile.pstats ..//..//src/hello_world.py
+python -m cProfile -o profile.pstats /../../src/hello_world.py
 gprof2dot -f pstats profile.pstats | ${graphviz}\\bin\\dot -Tpng -o out1.png
