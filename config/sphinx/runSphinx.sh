b8db5e283f5cae05b299a74d027faddd0a113d16
Sandeep Gupta <Sandeep.Gupta@Infineon.com>
2022-05-16 10:40:16 +0200

    Sphinx execution on Linux env

---
+++ config/sphinx/runSphinx.sh
@@ -0,0 +1,19 @@
+#!/bin/tcsh
+
+echo "Setting up the enviornment"
+
+module load python/3.9
+
+echo "Run this script to generate the Technical Doc"
+echo "This script expects sphinx and sphinx-rtd-theme python packages"
+echo "Python interpreter should be set when script is executed"
+
+echo "Setting up the enviornments for SPHINX"
+python -m pip install -r ../../config/python/requirements.txt
+
+echo  "Generate and edit reStructured text (.rst) files"
+sphinx-apidoc  -f  -o  ./source  ../../src
+
+echo "Generate the Technical Doc in HTML format"
+make html
+
