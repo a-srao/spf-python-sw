b244a70dfefe977b3e391aef110e501e409ffa05
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-17 08:42:04 +0530

    modified unittest.bat

--- scripts/windows/unittest.bat
+++ scripts/windows/unittest.bat
@@ -6,4 +6,4 @@ REM for the Python interpreter which is active when this script is being run
 python -m pip install -r config/python/requirements.txt
 coverage run --omit=*/test/* --source .\\src  --branch -m pytest --cache-clear --junitxml .\\test\\unit-test
 coverage html -d build_pytest/coverage_html
-coverage xml -o pytest.xml
+coverage xml -o .\\pytest.xml
