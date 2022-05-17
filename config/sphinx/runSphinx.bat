951ea27eb6319cd16004d22fd7d7e8a64540c9dc
Sandeep Gupta <Sandeep.Gupta@Infineon.com>
2022-05-17 18:20:50 +0200

    Fixing to run the script from root

--- config/sphinx/runSphinx.bat
+++ config/sphinx/runSphinx.bat
@@ -5,10 +5,13 @@ REM This script expects sphinx and sphinx-rtd-theme python packages
 REM Python interpreter should be set when script is executed
 
 REM Setting up the enviornments for SPHINX
-python -m pip install -r ../../config/python/requirements.txt
+python -m pip install -r config/python/requirements.txt
 
+cd config/sphinx
 REM Generate and edit reStructured text (.rst) files
 sphinx-apidoc  -f  -o  ./source  ../../src
 
 REM Generate the Technical Doc in HTML format
 make html
+cd ../..
+
--- config/sphinx/runSphinx.sh
+++ config/sphinx/runSphinx.sh
@@ -9,11 +9,13 @@ echo "This script expects sphinx and sphinx-rtd-theme python packages"
 echo "Python interpreter should be set when script is executed"
 
 echo "Setting up the enviornments for SPHINX"
-python -m pip install -r ../../config/python/requirements.txt
+python -m pip install -r config/python/requirements.txt
 
+cd config/sphinx
 echo  "Generate and edit reStructured text (.rst) files"
 sphinx-apidoc  -f  -o  ./source  ../../src
 
 echo "Generate the Technical Doc in HTML format"
 make html
+cd ../..
 
