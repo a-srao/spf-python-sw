5a3f3697aa8a3b708c97e77d992f88de3d6dd8a3
Amit Kumar <amit.kumar@infineon.com>
2022-05-18 11:37:47 +0530

    use pushd/popd instead of assuming directories

--- config/sphinx/runSphinx.sh
+++ config/sphinx/runSphinx.sh
@@ -10,12 +10,12 @@ echo "Python interpreter should be set when script is executed"
 
 echo "Setting up the enviornments for SPHINX"
 python -m pip install -r config/python/requirements.txt
-
+pushd `pwd`
 cd config/sphinx
 echo  "Generate and edit reStructured text (.rst) files"
-sphinx-apidoc  -f  -o  ./source  ../../src
+sphinx-apidoc  -f  -o  source  ../../src
 
 echo "Generate the Technical Doc in HTML format"
 make html
-cd ../..
+popd
 
