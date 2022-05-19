7f521539ef696149913de3a58f3a49447578f046
J Kishore (IFIN DES TCP SW TD / EE) <Kishore.J-EE@infineon.com>
2022-05-19 12:06:34 +0530

    added root path in bash script

--- scripts/linux/unittest.sh
+++ scripts/linux/unittest.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 module load python/3.9
 python -m pip install -r ../../config/python/requirements.txt
-python -m coverage run --omit=test --source src --branch -m pytest --html=report.html --cache-clear --junitxml pytest_hello/pytest.xml test/unit-test/
+python -m coverage run --omit=test --source src --branch -m pytest --html=report.html --cache-clear --junitxml pytest_hello/pytest.xml test/unit-test
 python -m coverage html -d pytest_shell/coverage_html
 python -m coverage xml -o pytest.xml
