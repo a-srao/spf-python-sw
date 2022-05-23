105434a7cd2ff8a38e8768e571f1b58fdd8847a1
Kumar Amit (IFIN DES TCP SW DEF) <Amit.Kumar@infineon.com>
2022-05-23 11:26:44 +0530

    print commands in unittest.sh

--- scripts/linux/unittest.sh
+++ scripts/linux/unittest.sh
@@ -1,6 +1,7 @@
 #!/bin/tcsh
+set echo
 module load python/3.9
 python -m pip install -r config/python/requirements.txt
-python -m coverage run --omit=*/test/* --source src  --branch -m pytest --html=reports/pytest.html --cache-clear --junitxml reports/pytest.xml test/unit-test
+python -m coverage run --omit=*/test/* --source src  --branch -m pytest --html=reports/pytest.html --cache-clear --junitxml reports/pytest.xml test\\unit-test
 python -m coverage html -d reports/coverage_html
 python -m coverage xml -o reports/coverage.xml
