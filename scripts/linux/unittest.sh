09505003029519f74dd0fae389bd8242f83f4363
Kumar Amit (IFIN DES TCP SW DEF) <Amit.Kumar@infineon.com>
2022-05-23 11:30:28 +0530

    correct directory in unittest.sh

--- scripts/linux/unittest.sh
+++ scripts/linux/unittest.sh
@@ -1,7 +1,6 @@
 #!/bin/tcsh
-set echo
 module load python/3.9
 python -m pip install -r config/python/requirements.txt
-python -m coverage run --omit=*/test/* --source src  --branch -m pytest --html=reports/pytest.html --cache-clear --junitxml reports/pytest.xml test/unit-test
-python -m coverage html -d reports/coverage_html
-python -m coverage xml -o reports/coverage.xml
+python -m coverage run --omit=test --source src --branch -m pytest --html=report.html --cache-clear --junitxml pytest_hello/pytest.xml test/unit-test
+python -m coverage html -d pytest_shell/coverage_html
+python -m coverage xml -o pytest.xml
