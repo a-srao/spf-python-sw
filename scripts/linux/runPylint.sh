a346cca460bb95e3d5a7a75ecf59dab2f3d0fe97
Sandeep Gupta <Sandeep.Gupta@Infineon.com>
2024-04-17 14:15:54 +0200

    Fixing issue for pylint execution

--- scripts/linux/runPylint.sh
+++ scripts/linux/runPylint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set echo
 set -efxo pipefail
 . /opt/Modules/init/bash
 
 source_files=${1?"ERROR: please pass 1st input as input source dir, eg src"}
-sonar_python_pylint_report=${2?"ERROR: please pass 2nd input as output source file, eg reports/pytest.xml"}
+sonar_python_pylint_report=${2?"ERROR: please pass 2nd input as output source file, eg reports/pylint.xml"}
 
-echo "source is $source_files"
-echo "source is $sonar_python_pylint_report"
+echo "input_source is $source_files"
+echo "output_source is $sonar_python_pylint_report"
 echo "This script to generate the static code analysis report using pylint"
 echo "This script expects pylint python packages"
 echo "Python interpreter should be set when script is executed"
