996db022402f04e65b4d8411141cd7cca2fd7775
Sandeep Gupta <Sandeep.Gupta@Infineon.com>
2024-04-22 14:32:57 +0200

    Fixing for unittest failure

--- scripts/linux/unittest.sh
+++ scripts/linux/unittest.sh
@@ -4,11 +4,13 @@ set -euxo pipefail
 . /opt/Modules/init/bash
 
 source_files=${source_files?"ERROR: Set the enviornment varaible as 1st input, eg src"}
-sonar_python_reportPath=${sonar_python_reportPath?"ERROR: Set the enviornment varaible as 2nd input, eg reports/pytest.html"}
-sonar_python_coverage_reportPath=${sonar_python_coverage_reportPath?"ERROR: Set the enviornment varaible as 3rd input, eg reports/coverage_html"}
+sonar_python_reportPath_html=${sonar_python_reportPath_html?"ERROR: Set the enviornment varaible as 2nd input, eg reports/pytest.html"}
+sonar_python_coverage_reportPath_html=${sonar_python_coverage_reportPath_html?"ERROR: Set the enviornment varaible as 3rd input, eg reports/coverage_html"}
+sonar_python_reportPath=${sonar_python_reportPath?"ERROR: Set the enviornment varaible as 4th input, eg reports/pytest.xml"}
+sonar_python_coverage_reportPath=${sonar_python_coverage_reportPath?"ERROR: Set the enviornment varaible as 5th input, eg reports/coverage_xml"}
 
 module load python/3.9
 python -m pip install -r config/python/requirements.txt
-python -m coverage run --omit=test --source $source_files  --branch -m pytest --html=$sonar_python_reportPath --cache-clear --junitxml $sonar_python_reportPath test/unit-test
-python -m coverage html -d $sonar_python_coverage_reportPath
+python -m coverage run --omit=test --source $source_files  --branch -m pytest --html=$sonar_python_reportPath_html --cache-clear --junitxml $sonar_python_reportPath test/unit-test
+python -m coverage html -d $sonar_python_coverage_reportPath_html
 python -m coverage xml -o $sonar_python_coverage_reportPath
