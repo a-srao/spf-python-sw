aaedc4c49cfdc49f17b7b1b01ac9709365413c46
a-srao <aishwaryasrao02@gmail.com>
2025-09-10 21:43:22 +0530

    Add Greetings

--- src/hello_world.py
+++ src/hello_world.py
@@ -8,9 +8,6 @@ def main():
     """Main Function"""
     print("Hello World!")
 
-def greet():
-    print("Greetings!")
-    print("Test Greet Function")
 
 
 # the backslash is there so that code coverage
